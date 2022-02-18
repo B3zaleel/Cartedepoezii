524477e35a0c20875c398f2a98308ac2f4839f70
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-02-18 09:25:16 +0100

    Add BaseModel and Comment model

---
+++ backend/models/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import os
+from datetime import datetime
+from sqlalchemy import create_engine
+from sqlalchemy import Boolean, Column, JSON, FLOAT, DATETIME, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = '{}://{}:{}@{}:{}/{}'.format(
+    os.getenv('DB_DIALECT_DRIVER'),
+    os.getenv('DB_USER'),
+    os.getenv('DB_PWORD'),
+    os.getenv('DB_HOST'),
+    os.getenv('DB_PORT'),
+    os.getenv('DB_NAME')
+)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+class BaseModel:
+    id = Column(String(64), unique=True, nullable=False, primary_key=True)
+    created_on = Column(DATETIME, nullable=False, default=datetime.utcnow())
+    updated_on = Column(DATETIME, nullable=False, default=datetime.utcnow())
+
+    def __init__(self, **kwargs):
+        for key, val in kwargs.items():
+            setattr(self, key, val)
---
+++ backend/models/comment.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+from sqlalchemy import Column, ForeignKey, TEXT, String
+from sqlalchemy.orm import relationship
+
+from . import Base, BaseModel
+
+
+class Comment(BaseModel, Base):
+    '''Represents a comment on a poem.'''
+    __tablename__ = 'comments'
+    poem_id = Column(
+        String(64),
+        ForeignKey('poems.id'),
+        nullable=False
+    )
+    user_id = Column(
+        String(64),
+        ForeignKey('users.id'),
+        nullable=False
+    )
+    comment_id = Column(
+        String(64),
+        ForeignKey('comments.id'),
+        nullable=True
+    )
+    text = Column(
+        TEXT,
+        nullable=False
+    )
