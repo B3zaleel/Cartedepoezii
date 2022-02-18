4ba7f1cfa0817ef2144947286604a4473cd06c6c
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-02-18 10:42:17 +0100

    Add middlewares

---
+++ backend/api/v1/middleware.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
+
+
+def inject_middlewares(app: FastAPI):
+    '''Adds all required middlewares to the given application.
+    '''
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*'],
+    )
+    app.add_middleware(GZipMiddleware, minimum_size=1024)
