43d852f470912d5c62e1d6e19b247186e5846b86
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-02-19 18:09:39 +0100

    Add and rename some routers

--- backend/api/v1/router.py
+++ backend/api/v1/router.py
@@ -2,10 +2,11 @@
 from fastapi import FastAPI
 
 from .routers import (
+    authentication,
     home_router,
-    auth,
-    media,
+    comment,
     poem,
+    search,
     user
 )
 
@@ -14,7 +15,8 @@ def inject_routers(app: FastAPI):
     '''Adds all required routers to the given application.
     '''
     app.include_router(home_router)
-    app.include_router(auth.router)
-    app.include_router(media.router)
+    app.include_router(authentication.router)
+    app.include_router(comment.router)
     app.include_router(poem.router)
+    app.include_router(search.router)
     app.include_router(user.router)
