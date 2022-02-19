94d04b6622aa4a347eaea411cfdfb2eb9c974e4b
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-02-20 00:12:31 +0100

    Refactor comment SQLAlchemy model

--- backend/models/comment.py
+++ backend/models/comment.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
-from sqlalchemy import Column, ForeignKey, TEXT, String
-from sqlalchemy.orm import relationship
+from datetime import datetime
+from sqlalchemy import Column, ForeignKey, TIMESTAMP, String
 
-from . import Base, BaseModel
+from . import Base
 
 
-class Comment(BaseModel, Base):
-    '''Represents a comment on a poem.'''
+class Comment(Base):
+    '''Represents a comment on a poem or reply to a comment.'''
     __tablename__ = 'comments'
+    id = Column(
+        String(64),
+        unique=True,
+        nullable=False,
+        primary_key=True
+    )
+    created_on = Column(
+        TIMESTAMP(True),
+        nullable=False,
+        default=datetime.utcnow()
+    )
     poem_id = Column(
         String(64),
         ForeignKey('poems.id'),
