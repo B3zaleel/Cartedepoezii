894093eda79172fafb1611094a0a704b93ee37fe
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-02-21 08:55:54 +0100

    Add icons

---
+++ web_client/src/assets/icons/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    version="1.1"
+    id="mdi-home"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24">
+    <path d="M10,20V14H14V20H19V12H22L12,3L2,12H5V20H10Z" />
+  </svg>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+}) export default class HomeIcon extends Vue {}
+</script>
+
+<style></style>
