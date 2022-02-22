61f1776fe4623ff457af23928db673332676f172
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-02-22 19:05:51 +0100

    Create User home page

---
+++ web_client/src/views/UserHome.vue
@@ -0,0 +1,44 @@
+<template>
+  <MainLayout>
+    <template v-slot:header>
+      <div class="header-pane">
+        <h2>Home</h2>
+      </div>
+    </template>
+
+    <template v-slot:main>
+      <div class="user-home">
+        <div>
+          <div>
+            <Poem/>
+          </div>
+        </div>
+      </div>
+    </template>
+  </MainLayout>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import MainLayout from '@/views/layout/Main.vue';
+import Poem from '@/components/Poem.vue';
+
+@Component({
+  name: 'UserHomeView',
+  components: {
+    MainLayout,
+    Poem,
+  },
+})
+export default class UserHomeView extends Vue {}
+</script>
+
+<style lang="scss">
+.header-pane {
+  > h2 {
+    margin: 0;
+    font-size: 1.2em;
+    cursor: context-menu;
+  }
+}
+</style>
