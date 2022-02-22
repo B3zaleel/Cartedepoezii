073d4b8d362da84b6581f35b7a068511a752568b
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-02-22 19:13:11 +0100

    Update sign in mutation in store

--- web_client/src/store/index.ts
+++ web_client/src/store/index.ts
@@ -5,8 +5,30 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    API_URL: 'http://localhost:5000/api/v1',
+    user: {
+      isAuthenticated: window.localStorage.getItem('user.isAuthenticated') === 'true',
+      authToken: window.localStorage.getItem('user.authToken') || '',
+      id: window.localStorage.getItem('user.id') || '',
+    },
   },
   mutations: {
+    signIn(state, info) {
+      state.user.authToken = info.token;
+      state.user.id = info.id;
+      state.user.isAuthenticated = true;
+      window.localStorage.setItem('user.authToken', info.token);
+      window.localStorage.setItem('user.id', info.id);
+      window.localStorage.setItem('user.isAuthenticated', 'true');
+    },
+    signOut(state) {
+      state.user.authToken = '';
+      state.user.id = '';
+      state.user.isAuthenticated = false;
+      window.localStorage.setItem('user.authToken', '');
+      window.localStorage.setItem('user.id', '');
+      window.localStorage.setItem('user.isAuthenticated', 'false');
+    },
   },
   actions: {
   },
