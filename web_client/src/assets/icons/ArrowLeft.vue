63272519b1542cc0b7f5cda162dec4604cb3b9e1
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-02-23 11:14:14 +0100

    Add UI icons

---
+++ web_client/src/assets/icons/ArrowLeft.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    version="1.1"
+    id="mdi-arrow-left"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24">
+    <path d="M20,11V13H8L13.5,18.5L12.08,19.92L4.16,12L12.08,4.08L13.5,5.5L8,11H20Z" />
+  </svg>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+}) export default class ArrowLeftIcon extends Vue {}
+</script>
+
+<style></style>
