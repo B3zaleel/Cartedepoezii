27749635636a486aa604466c2207d49de15f0fa7
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-02-23 11:08:18 +0100

    Create guest home page view

---
+++ web_client/src/views/GuestHome.vue
@@ -0,0 +1,131 @@
+<template>
+  <MainAltLayout>
+    <div class="guest-home">
+      <div class="main">
+        <div>
+          <img
+            class="bg"
+            loading="lazy"
+            src="../assets/images/flat-lay-out-dark-green-leafs.jpg"
+          />
+          <div class="bg">
+            <Logo/>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <h1>It's poetic</h1>
+            <h2>Join Cartedepoezii today.</h2>
+            <router-link class="btn-link bold" to="/sign-up">Sign up with email</router-link>
+            <div>
+              <h4>
+                By signing up, you agree to our
+                <router-link to="/terms-of-service">Terms of service</router-link>
+                and <router-link to="privacy-policy">Privacy Policy</router-link>.
+              </h4>
+            </div>
+          </div>
+
+          <div>
+            <h3>Already have an account?</h3>
+            <router-link class="btn-link" to="/sign-in">Sign In</router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </MainAltLayout>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import MainAltLayout from '@/views/layout/MainAlt.vue';
+import Logo from '@/assets/icons/Logo.vue';
+
+@Component({
+  name: 'GuestHomeView',
+  components: {
+    MainAltLayout,
+    Logo,
+  },
+})
+export default class GuestHomeView extends Vue {}
+</script>
+
+<style lang="scss">
+.guest-home {
+  display: grid;
+  grid-template-rows: 1fr auto;
+  min-height: 100%;
+
+  .main {
+    display: grid;
+    grid-template-columns: 50% 50%;
+
+    div {
+      position: relative;
+
+      img {
+        width: 100%;
+        object-fit: cover;
+      }
+      .bg{
+        position: absolute;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100%;
+        left: 0;
+        bottom: 0;
+      }
+      .bg:last-child{
+        z-index: 5;
+        background: #b4f0dec4;
+        backdrop-filter: blur(1.5px);
+
+        > svg {
+          width: clamp(30px, 80%, 720px);
+          height: clamp(30px, 80%, 720px);
+        }
+      }
+    }
+
+    > div:nth-child(2) {
+      padding: 20px;
+    }
+
+    h3 {
+      font-size: 0.7em;
+    }
+
+    h4 {
+      font-size: 0.6em;
+    }
+  }
+}
+
+@media (max-width: 600px) {
+  .guest-home {
+    .main {
+      display: flex;
+      flex-flow: column-reverse;
+      grid-template-columns: none;
+      align-items: stretch;
+      justify-content: stretch;
+
+      > div:first-child {
+        min-height: 150px;
+        height: 40%;
+      }
+
+      > div:last-child {
+        height: inherit;
+        align-items: center;
+        height: 60%;
+        min-height: 250px;
+      }
+    }
+  }
+}
+</style>
