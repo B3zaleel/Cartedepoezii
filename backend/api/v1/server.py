24fe82c17017798a8e0757fa03588ba1a953c2aa
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-02-23 11:22:27 +0100

    Add eerror handlers for the API server

--- backend/api/v1/server.py
+++ backend/api/v1/server.py
@@ -2,6 +2,10 @@
 import os
 import uvicorn
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
 
 from .router import inject_routers
 from .middleware import inject_middlewares
@@ -10,6 +14,26 @@ from .middleware import inject_middlewares
 app = FastAPI()
 inject_middlewares(app)
 inject_routers(app)
+
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    default_message = 'Request failed.'
+    res = {
+        'success': False,
+        'message': str(exc) if exc else default_message
+    }
+    return JSONResponse(res)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    default_message = 'Request failed.'
+    res = {
+        'success': False,
+        'message': str(exc) if exc else default_message
+    }
+    return JSONResponse(res)
 
 
 if __name__ == '__main__':
