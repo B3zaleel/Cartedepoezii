568789ce9cfab702099bf609e674d7811a6f984f
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-02-24 09:30:07 +0100

    Update number formatting function

--- web_client/src/assets/scripts/math_utils.ts
+++ web_client/src/assets/scripts/math_utils.ts
@@ -5,12 +5,16 @@ export default class MathUtils {
     let rem = 0;
     let total = num;
     const base = 1000;
+    let useDecimal = withDecimal;
 
     while (total > base) {
       rem = Math.floor((total % base) / 100);
       total = Math.floor(total / base);
       idx += 1;
     }
-    return withDecimal ? `${total}.${rem}${ranks[idx]}` : `${total}${ranks[idx]}`;
+    if (!rem) {
+      useDecimal = false;
+    }
+    return useDecimal ? `${total}.${rem}${ranks[idx]}` : `${total}${ranks[idx]}`;
   };
 }
