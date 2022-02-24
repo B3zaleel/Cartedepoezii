9b46c26dbf40f51f88be220a69edacab17999067
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-02-24 09:35:29 +0100

    Update App view

--- web_client/src/App.vue
+++ web_client/src/App.vue
@@ -1,32 +1,80 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+@use "@/assets/styles/globals";
+// @font-face{
+//   src: url("@/assets/fonts/sans-serif/Quicksand-VariableFont_wght.ttf") format("truetype");
+//   font-family: CartedepoeziiFont;
+// }
+
+// @font-face{
+//   src: url("@/assets/fonts/serif/ZenAntique-Regular.ttf") format("truetype");
+//   font-family: CartedepoeziiSerifFont;
+// }
+
+:root {
+  --primary-color-light: #537494;
+  --primary-color: #2c3e50;
+}
+
+html, body {
+  margin: 0;
+  height: 100%;
+  overscroll-behavior: auto none;
+}
+
+* {
+  font-family: globals.$Sans-Serif-Font;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  font-family: globals.$Serif-Font;
+  margin: 0;
+}
+
+h1 {
+  font-size: clamp(12px, 1.2em, 15px);
+}
+
+h2 {
+  font-size: clamp(12px, 1.2em, 15px);
+}
+
 #app {
+  display: contents;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: var(--primary-color);
+  min-height: 100%;
+
+  > .content-container {
+    display: contents;
+    min-height: 100%;
+  }
 }
 
-#nav {
-  padding: 30px;
+.btn-link {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  padding: 4px 12px;
+  border: 1px solid gray;
+  border-radius: 16px;
+  text-decoration: none;
+  cursor: pointer;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.btn-link.bold {
+  color: white;
+  background: #2c3e50;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.btn-link:hover {
+  opacity: 0.8;
 }
 </style>
