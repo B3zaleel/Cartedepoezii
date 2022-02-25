f6c9845476fba0a566d80ade42385b0e224cdb83
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-02-25 11:52:38 +0100

    Add more icons

---
+++ web_client/src/assets/icons/Plus.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    version="1.1"
+    id="mdi-plus"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
+  </svg>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+}) export default class PlusIcon extends Vue {}
+</script>
+
+<style></style>
