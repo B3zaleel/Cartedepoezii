67eb193606486e7a3c71c4309824e6697c4eb11f
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-02-25 12:44:53 +0100

    Create Explore page view

---
+++ web_client/src/views/Explore.vue
@@ -0,0 +1,51 @@
+<template>
+  <MainLayout>
+    <template v-slot:header>
+      <div class="header-pane">
+        <div class="search-pane">
+          <input
+            placeholder="Search Cartedepoezii"
+            v-model="searchQuery"
+          />
+          <button class="cdp-btn icon">
+            <MagnifyIcon/>
+          </button>
+        </div>
+      </div>
+    </template>
+
+    <template v-slot:main>
+      <div class="app-explore">
+        <div>
+          <div>
+            <!-- <Poem/> -->
+          </div>
+        </div>
+      </div>
+    </template>
+  </MainLayout>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { Poem as PoemT } from '@/assets/scripts/type_defs';
+import MainLayout from '@/views/layout/Main.vue';
+import Poem from '@/components/Poem.vue';
+import MagnifyIcon from '@/assets/icons/Magnify.vue';
+
+@Component({
+  name: 'ExploreView',
+  components: {
+    MainLayout,
+    Poem,
+    MagnifyIcon,
+  },
+})
+export default class ExploreView extends Vue {
+  searchQuery = '';
+}
+</script>
+
+<style lang="scss">
+@use "@/assets/styles/views/search";
+</style>
