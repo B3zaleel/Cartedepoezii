df8da695c019138e4aa39f09254d76ee0ef9f454
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-03-03 14:26:35 +0100

    Change PUT: /user endpoint specification

--- backend/api/v1/form_types.py
+++ backend/api/v1/form_types.py
@@ -29,6 +29,17 @@ class UserDeleteForm(BaseModel):
     userId: str
 
 
+class UserUpdateForm(BaseModel):
+    authToken: str
+    userId: str
+    name: str
+    profilePhoto: Optional[str]
+    profilePhotoId: str
+    removeProfilePhoto: bool
+    email: str
+    bio: str
+
+
 class ConnectionForm(BaseModel):
     authToken: str
     userId: str
