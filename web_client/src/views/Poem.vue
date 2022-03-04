0168b1fd41a2d558a04ffcd8443b935426e72edc
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-03-04 19:23:46 +0100

    Integrate API in Poem view

--- web_client/src/views/Poem.vue
+++ web_client/src/views/Poem.vue
@@ -18,15 +18,17 @@
     <template v-slot:main>
       <div class="user-poem">
         <div>
-          <div>
-            <Poem :poem="poem"/>
-          </div>
-          <div class="{loading: true}">
+          <div v-show="!poemLoaded" class="loading-pane">
             <LoadingIcon/>
           </div>
-        </div>
-        <div>
-          <div>
+          <div v-show="poemLoaded">
+            <Poem :poem="poem"/>
+          </div>
+          <div v-show="poemLoaded">
+            <ItemsLoaderLayout
+              :itemsName="'comments'"
+              :itemsFetcher="commentsFetcher"
+            />
           </div>
         </div>
       </div>
@@ -36,12 +38,15 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import { UserMin, Poem as PoemT } from '@/assets/scripts/type_defs';
+import { Page, Item } from '@/assets/scripts/types/interfaces';
+import CommentAPIReq from '@/assets/scripts/api_requests/comment';
+import PoemAPIReq from '@/assets/scripts/api_requests/poem';
+import PoemT from '@/assets/scripts/types/poem';
 import ArrowLeftIcon from '@/assets/icons/ArrowLeft.vue';
 import LoadingIcon from '@/assets/icons/Loading.vue';
 import MainLayout from '@/views/layout/Main.vue';
+import ItemsLoaderLayout from '@/views/layout/ItemsLoader.vue';
 import Poem from '@/components/Poem.vue';
-import Comment from '@/components/Comment.vue';
 
 @Component({
   name: 'PoemView',
@@ -49,29 +54,57 @@ import Comment from '@/components/Comment.vue';
     ArrowLeftIcon,
     LoadingIcon,
     MainLayout,
+    ItemsLoaderLayout,
     Poem,
-    Comment,
   },
 })
 export default class PoemView extends Vue {
-  poem = {
-    id: 'ffjddfjf',
-    publishedDate: new Date(),
-    title: '',
-    verses: ['T\xa0h\nis\nis\na d\nem\no wo\nrk', '4553\n\xa0\n\xa0\n\xa0\n\xa0\n\xa0\n\xa0...'],
-    user: {
-      id: 'lk-89iii',
-      name: 'Beza',
-      profilePhotoId: '',
-      isFollowing: false,
-    },
-    isLiked: false,
-    likesCount: 15,
-    commentsCount: 5,
-  };
+  poem: PoemT = new PoemT();
+
+  poemLoaded = false;
+
+  commentsSrc = '';
+
+  commentsQueryParams = '';
+
+  commentAPIReq = new CommentAPIReq(
+    this.$store.state.API_URL,
+    this.$store.state.user.authToken,
+  );
+
+  poemAPIReq = new PoemAPIReq(
+    this.$store.state.API_URL,
+    this.$store.state.user.authToken,
+  );
+
+  commentsFetcher(page: Page): Promise<{
+      success: boolean,
+      data?: Array<Item>,
+      message?: string
+    }> {
+    return this.commentAPIReq.getPoemComments(this.$route.params.id, page);
+  }
+
+  loadPoem(): void {
+    this.poemLoaded = false;
+    this.poemAPIReq.getPoem(this.$route.params.id)
+      .then((res) => {
+        if (res.success) {
+          if (res.data) {
+            this.poem = res.data;
+            document.title = `${this.poem.user.name}: ${this.poem.title} - Cartedepoezii`;
+            this.poemLoaded = true;
+          }
+        }
+      });
+  }
+
+  mounted(): void {
+    this.loadPoem();
+  }
 }
 </script>
 
 <style lang="scss">
-// @use '@/assets/styles/views/profile';
+@use '@/assets/styles/views/poem';
 </style>
