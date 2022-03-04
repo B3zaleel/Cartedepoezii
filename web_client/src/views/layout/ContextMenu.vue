10a584488990ef82108e050911f51825f198fd53
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-03-04 14:32:46 +0100

    Add support for custom positioning

--- web_client/src/views/layout/ContextMenu.vue
+++ web_client/src/views/layout/ContextMenu.vue
@@ -1,6 +1,7 @@
 <template>
   <div
     :class="{menu: true, visible: isMenuOpen,}"
+    :style="getPositionStyle()"
     @mousedown="mouseDownEvent"
   >
     <slot></slot>
@@ -9,6 +10,7 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Position } from '@/assets/scripts/types/interfaces';
 
 @Component({
   name: 'ContextMenuLayout',
@@ -21,6 +23,23 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 }) export default class ContextMenuLayout extends Vue {
   @Prop() isMenuOpen!: boolean;
 
+  @Prop() position: Position = {
+    type: 'absolute',
+    right: '0',
+    top: '0',
+  };
+
+  getPositionStyle(): Record<string, string> {
+    const style = {
+      position: this.position.type,
+      left: this.position.left || 'auto',
+      top: this.position.top || 'auto',
+      right: this.position.right || 'auto',
+      bottom: this.position.bottom || 'auto',
+    };
+    return style;
+  }
+
   created(): void {
     window.addEventListener('mousedown', () => {
       this.$emit('request-close');
@@ -30,20 +49,19 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 </script>
 
 <style lang="scss">
+@use "@/assets/styles/globals";
+
 .menu.visible {
   display: block;
 }
 
 .menu {
-  position: absolute;
   display: none;
-  right: 0;
-  top: 0;
   padding: 5px 0;
   border-radius: 4px;
   border: 1px solid gray;
   background: ghostwhite;
-  z-index: 5;
+  z-index: 700;
   box-shadow: 0 0 2px 1.5px #a3a3a3dc;
 
   > div {
@@ -51,17 +69,22 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
     flex-flow: column;
 
     .menu-item {
+      display: flex;
       padding: 5px;
+      color: black;
       background: none;
       border: none;
+      white-space: nowrap;
+      text-decoration: none;
       cursor: pointer;
 
-      &:not(:first-child) {
-        border-top: 1px solid gainsboro;
-      }
-
       &:hover {
-        background: gainsboro;
+        color: white;
+        background: globals.$primary-color-light;
+
+        &.danger {
+          background: globals.$danger-color-light;
+        }
       }
     }
   }
