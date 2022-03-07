ff0474535b0db6397210b289ac4268f7350a9aee
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-03-07 12:13:32 +0100

    Create Error404 page view

---
+++ web_client/src/views/error/_Error404.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="error-pane">
+    <h1>Oh No!</h1>
+    <p>
+      We searched far and wide into the mist,<br/>
+      But nothing came from within.<br/>
+      We waited until the clear skies turned grey.<br/>
+      After so long, back <router-link class="cdp-btn text" to="/">home</router-link> we went.
+    </p>
+  </div>
+</template>
+
+<script lang='ts'>
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'Error404Content',
+})
+export default class Error404Content extends Vue {}
+</script>
