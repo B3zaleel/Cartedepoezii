5496e34aa8d77801b431b1641fd23f659d42e62e
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-03-14 19:11:10 +0100

    Update About page view

--- web_client/src/views/info/About.vue
+++ web_client/src/views/info/About.vue
@@ -1,57 +1,198 @@
 <template>
   <div class="content-container">
-    <MainLayout v-if="isAuthenticated()">
-      <template v-slot:header>
-        <div class="header-pane">
-          <div class="page-nav-bar">
+    <main>
+      <div class="header">
+        <div>
+          <a href="/">
+            <Logo/>
+          </a>
+        </div>
+
+        <nav>
+          <a class="nav-link" href="#features-section">
+            <span>Features</span>
+          </a>
+          <a class="nav-link" href="#about-section">
+            <span>About</span>
+          </a>
+          <button class="nav-link" @click="visitSite">
+            <LaunchIcon/>
+            <span>Visit Site</span>
+          </button>
+
+          <button class="nav-launch-btn">
+            <DotsHexagonIcon/>
+          </button>
+        </nav>
+      </div>
+
+      <article class='app-about'>
+        <div>
+          <div>
+            <img/>
+          </div>
+
+          <div>
+            <Logo/>
+            <h3>
+              Cartedepoezii
+            </h3>
+            <span>
+              Create and find the poems you love
+            </span>
+          </div>
+        </div>
+
+        <div id="features-section">
+          <div
+            class="sect feature"
+            v-for="feature, i in features"
+            :key="i"
+          >
             <div>
-              <button class="cdp-btn icon" @click="goBack">
-                <ArrowLeftIcon/>
-              </button>
+              <img :src="feature.imgSrc ? require(feature.imgSrc) : ''"/>
             </div>
+
             <div>
-              <h1>About Cartedepoezii</h1>
+              <b class="title">{{ feature.title }}</b>
+              <p class="descr">{{ feature.description }}</p>
             </div>
           </div>
         </div>
-      </template>
 
-      <template v-slot:main>
-        <MainContent/>
-      </template>
-    </MainLayout>
-    <MainAltLayout v-if="!isAuthenticated()">
-      <MainContent/>
-    </MainAltLayout>
+        <div class="mini-sect">
+          <h3>
+            Join us today
+          </h3>
+          <a
+            class="cdp-btn text"
+            href="/sign-up"
+            target="_blank"
+          >
+            Sign Up
+          </a>
+        </div>
+
+        <div id="about-section" class="sect dark-bg">
+          <div>
+            <p>
+              foo
+            </p>
+          </div>
+
+          <div>
+            <div
+              class="contributor-card"
+              v-for="contributor, i in contributors"
+              :key="i"
+            >
+              <img :src="contributor.profilePhotoSrc"/>
+              <b class="name">{{ contributor.name }}</b>
+              <div class="socials">
+                <a
+                  v-for="social in contributor.socials"
+                  :key="social.name"
+                  :href="social.src"
+                  target="_blank"
+                >
+                  <GitHubIcon v-if="social.name === 'GitHub'"/>
+                  <TwitterIcon v-if="social.name === 'Twitter'"/>
+                  <LinkedInIcon v-if="social.name === 'LinkedIn'"/>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="footer">
+          <div>
+            <a href="/terms-of-service">Terms</a>
+            <a href="/privacy-policy">Privacy</a>
+          </div>
+
+          <div>
+            <a
+              href="https://github.com/B3zaleel/Cartedepoezii/discussions/categories/q-a"
+              target="_blank"
+            >
+              <HelpCircleIcon/>
+              <span>Help</span>
+            </a>
+          </div>
+        </div>
+      </article>
+    </main>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import MainLayout from '@/views/layout/Main.vue';
-import MainAltLayout from '@/views/layout/MainAlt.vue';
-import ArrowLeftIcon from '@/assets/icons/ArrowLeft.vue';
-import MainContent from './_About.vue';
+import Logo from '@/assets/icons/Logo.vue';
+import LaunchIcon from '@/assets/icons/Launch.vue';
+import DotsHexagonIcon from '@/assets/icons/DotsHexagon.vue';
+import GitHubIcon from '@/assets/icons/social/GitHub.vue';
+import TwitterIcon from '@/assets/icons/social/Twitter.vue';
+import LinkedInIcon from '@/assets/icons/social/LinkedIn.vue';
+import HelpCircleIcon from '@/assets/icons/HelpCircle.vue';
 
 @Component({
   name: 'AboutView',
+  methods: {
+    visitSite() {
+      window.open('/', '_blank');
+    },
+  },
   components: {
-    MainLayout,
-    MainAltLayout,
-    ArrowLeftIcon,
-    MainContent,
+    Logo,
+    LaunchIcon,
+    DotsHexagonIcon,
+    GitHubIcon,
+    TwitterIcon,
+    LinkedInIcon,
+    HelpCircleIcon,
   },
 })
 export default class AboutView extends Vue {
   userId! : string
 
-  isAuthenticated(): boolean {
-    return this.$store.state.user.isAuthenticated;
-  }
+  features = [
+    {
+      title: 'Post poems',
+      description: '',
+      imgSrc: '',
+    },
+    {
+      title: 'Join the conversation',
+      description: '',
+      imgSrc: '../../assets/images/info_about/Cartedepoezii_Feature_1.webp',
+    },
+    {
+      title: 'Join the conversation',
+      description: '',
+      imgSrc: '',
+    },
+  ];
 
-  goBack(): void {
-    this.$router.go(-1);
-  }
+  contributors = [
+    {
+      name: 'Bezaleel Olakunori',
+      profilePhotoSrc: 'https://avatars.githubusercontent.com/u/17376603?v=4',
+      socials: [
+        {
+          name: 'GitHub',
+          src: 'https://github.com/B3zaleel',
+        },
+        {
+          name: 'Twitter',
+          src: 'https://twitter.com/B3zaleel',
+        },
+        {
+          name: 'LinkedIn',
+          src: 'https://www.linkedin.com/in/bezaleel-olakunori-34335513a/',
+        },
+      ],
+    },
+  ];
 
   created(): void {
     this.userId = this.$route.params.id;
@@ -60,5 +201,5 @@ export default class AboutView extends Vue {
 </script>
 
 <style lang="scss">
-@use "@/assets/styles/views/info";
+@use "@/assets/styles/views/info/about";
 </style>
