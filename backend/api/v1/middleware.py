aca1dc7a56937ace17cbbbd99b2ba4b2429ff0ce
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-03-14 22:32:17 +0100

    Update middlewares

--- backend/api/v1/middleware.py
+++ backend/api/v1/middleware.py
@@ -10,7 +10,7 @@ def inject_middlewares(app: FastAPI):
     app.add_middleware(
         CORSMiddleware,
         allow_origins=['*'],
-        allow_credentials=True,
+        allow_credentials=False,
         allow_methods=['*'],
         allow_headers=['*'],
     )
