1034adb5b49ad60d5a01359f294f8da8b8bffdcc
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-03-15 04:26:56 +0100

    Update middlewares

--- backend/api/v1/middleware.py
+++ backend/api/v1/middleware.py
@@ -9,9 +9,9 @@ def inject_middlewares(app: FastAPI):
     '''
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=['*'],
-        allow_credentials=False,
+        allow_origins=['https://cartedepoezii.netlify.app'],
+        allow_credentials=True,
         allow_methods=['*'],
-        allow_headers=['*'],
+        allow_headers=['*']
     )
     app.add_middleware(GZipMiddleware, minimum_size=1024)
